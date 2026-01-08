2bdf9b9b12d2481244e95df8cc38deac9a55cf48
codalee123 <olawunmibello1@gmail.com>
2026-01-08 14:58:58 -0800

    Skygrit link updated

--- src/component/ProductDropdownCard.jsx
+++ src/component/ProductDropdownCard.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const ProductDropdownCard = () => {
   return (
-    <div className="bg-[#20113c] font-inter font-normal text-white px-4 pt-5 rounded-lg shadow-xl w-max max-lg:shadow-[inset_0_0_0_2px_rgba(255,255,255,0.21)] max-md:mx-0 max-lg:w-full">
+    <a href="https://skygrit.juneinfra.com/"
+      target="_blank" rel="noopener" 
+      className="bg-[#20113c] font-inter font-normal text-white px-4 pt-5 rounded-lg shadow-xl w-max max-lg:shadow-[inset_0_0_0_2px_rgba(255,255,255,0.21)] max-md:mx-0 max-lg:w-full">
       <p className='text-base max-lg:text-base '>Get instant access to our products and tools to create, scale, and lead.</p>
 
       <blockquote className='bg-[#f4f4f4] rounded-lg p-4 my-5'>
@@ -11,7 +13,7 @@ const ProductDropdownCard = () => {
           Revolutionizing Post-Booking Airline Customer Experience in Nigeria and Africa
         </p>
       </blockquote>
-    </div>
+    </a>
   );
 };
 
